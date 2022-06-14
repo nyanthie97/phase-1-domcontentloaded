ec77282a212fe4956e74f324613f03543b32b17c
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-15 00:53:38 +0300

    commit tree f1d5a5e778baa2818f23f3c3c7cc46c8c2dcf26d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+
+      document.addEventListener('DOMContentLoaded', ()=> {
+        document.querySelector('#text').textContent = 'This is really cool!';
+    }) 
+  
